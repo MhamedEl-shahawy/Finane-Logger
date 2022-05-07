a664072537f8f01bd36ee817b89b05e47345ec48
MhamedElshahawy <mohamedelshahawy599@gmail.com>
2022-05-07 18:20:04 +0200

    first release

---
+++ public/sandbox.js
@@ -0,0 +1,9 @@
+"use strict";
+const character = 'mario';
+const list = [];
+const subList = [];
+console.log(character);
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
+    console.log(input);
+});
---
+++ src/sandbox.ts
@@ -0,0 +1,10 @@
+const character:string = 'mario';
+const list:string[]= [];
+const subList:(string|number)[] = [];
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
